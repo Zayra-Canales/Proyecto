18ff5ae90783bbac2f703168c0dcb30a49ae83c7
Zayra-Canales <zayricanales@hotmail.com>
2019-09-12 22:48:08 -0600

    empresa

--- ProyectoZAYRA/registro-empresa.php
+++ ProyectoZAYRA/registro-empresa.php
@@ -3,18 +3,18 @@
 <head>
 	<meta charset="utf-8">
 	<title>BRYZATEC</title>
-	<!--- Estilos De Bootstrap-->
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 	<!--- Estilos fontawesome -->
 	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
 	<!--- Estilos Css -->
 	<link rel="stylesheet" type="text/css" href="css/creative.min.css">
+    <!--- Estilos De Bootstrap-->
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 		<!--- Estilos Css -->
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 </head>
 <body>
 	<!-- Inicio de Codificacion-->
-
+<!-- 
 	   <nav class="navbar navbar-expand-lg bg-success fixed-top py-3" id="mainNav">
     <div class="container">
       <a class="navbar-brand js-scroll-trigger title text-white nav-main" href="main.html">BRYZATEC</a>
@@ -42,31 +42,43 @@
     </div>
   </nav>
 <br><br><br><br><br>
+-->
 
-<div class="container">
-  <h3 class="text-center mb-4" >Registra empresa</h3>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Nombre empresa</label>
-      <input type="email" class="form-control" id="correoEmpresa" placeholder="nombre empresa">
+<div class="container-fluid mt-3 mb-5">
+
+  <div class="row">
+
+    <div class="col-lg-8 col-xl-8 col-md-12 logo-empresa ml-2 navbar-brand" style="background-image: url('img/logoEmpresa.jpg'); ">
     </div>
-    <div class="form-group col-md-6">
+
+    <div class="col-lg-3 col-xl-3 col-md-12 col-xs-12 ">
+        <h3 class="text-center text-success titulo">Registrar Empresa</h3><br>
+      <div class="form-grou grupo ">
+      <label class="label" for="inputEmail4">Nombre empresa</label>
+      <input type="email" class="form-contro imput" id="correoEmpresa" placeholder="nombre empresa">
+    </div>
+
+    <div class="form-group">
       <label for="inputPassword4">Direccion</label>
       <input type="password" class="form-control" id="direccionEmpresa" placeholder="Direccion">
     </div>
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
+
+    <div class="form-group">
       <label for="inputEmail4">Contraseña</label>
       <input type="email" class="form-control" id="contraEmpresa" placeholder="contraseña">
     </div>
-    <div class="form-group col-md-6">
+
+    <div class="form-group">
       <label for="inputPassword4">Confirmar Contraseña</label>
       <input type="password" class="form-control" id="contraEmpresa2" placeholder="confirmar contraseña">
     </div>
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-4">
+
+    <div class="form-group">
+      <label for="inputPassword4">Confirmar Contraseña</label>
+      <input type="password" class="form-control" id="contraEmpresa2" placeholder="confirmar contraseña">
+    </div>
+
+    <div class="form-group">
       <label for="inputState">Pais</label>
       <select id="paisEmpresa" class="form-control">
         <option selected>Choose...</option>
@@ -75,9 +87,14 @@
         <option value="3">Mexico</option>
       </select>
     </div>
-  </div>
-  <button id="btn-agregarEmpresa" type="button" class="btn btn-warning">Registrar</button>    
-  </div>
+
+    <br>
+    <button class="boton btn-block">Agregar</button>
+
+
+    </div>
+    
+ 
 
 
 	<!-- Fin de codificacion-->
